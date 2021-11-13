7a98138a98a47783033fa8d015b42ace9a946821
damatgre <gregdamato2@gmail.com>
2021-11-13 16:05:51 -0500

    porfolio list component styled and dynamic

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,10 +1,84 @@
-import React from 'react';
+
+import { React, useState } from 'react'
 import './portfolio.scss';
+import PortfolioList from '../portfolioList/portfoliolist';
 
 export default function Portfolio() {
+
+    const [selected, setSelected] = useState('featured')
+
+    const list = [
+        {
+            id: 'featured',
+            title: 'Featured',
+        },
+        {
+            id: 'web',
+            title: 'Web App',
+        },
+        {
+            id: 'mobile',
+            title: 'Mobile App',
+        },
+        {
+            id: 'design',
+            title: 'Design',
+        },
+        {
+            id: 'branding',
+            title: 'Branding',
+        },
+        {
+            id: 'content',
+            title: 'Content'
+        }
+    ]
+
     return (
         <div className='portfolio' id='portfolio'>
-            Portfolio
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => (
+                    <PortfolioList 
+                    title={item.title} 
+                    active={selected === item.id}
+                    setSelected={setSelected}
+                    id={item.id}
+                    />
+                ))}
+            </ul>
+            <div className='container'>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+                <div className='item'>
+                    <img src="https://images.ctfassets.net/hrltx12pl8hq/5GaLeZJlLyOiQC4gOA0qUM/a0398c237e9744ade8b072f99349e07a/shutterstock_152461202_thumb.jpg?fit=fill&w=480&h=270"
+                        alt='' />
+                    <h3>Application</h3>
+                </div>
+            </div>
         </div>
     )
 }
