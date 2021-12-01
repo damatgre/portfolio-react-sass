f901281f93d4c1457c485ab2441b8ade9446c7b2
damatgre <gregdamato2@gmail.com>
2021-11-30 19:57:02 -0500

    styling project slider

--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -2,9 +2,59 @@ import React from 'react'
 import './work.scss'
 
 export default function Work() {
+
+    const data = [
+        {
+            id: 1,
+            title: "What's For Dinner?",
+            desc: "A front end application that makes the difficult decision of what’s for dinner into a simple one. Users choose between our Eat-In and Eat-Out options to obtain a recipe, or the information of local restaurants.",
+            img: "https://i0.wp.com/visualmeringue.com/wp-content/uploads/2018/02/WhatsForDinner_MealPlanner_Flat_8236.jpg?resize=800%2C534&ssl=1"
+        },
+        {
+            id: 2,
+            title: "What I Had For Dinner",
+            desc: "What I Had For Dinner is a recipe blog that engages the user to find new and exciting recipes. Users can check out posts from the community, and create their own posts to store recipes they want to use again! Think of it as your own personalized recipe book that helps make sure you don't lose any amazing recipes you've found online.",
+            img: "https://media.istockphoto.com/photos/taking-a-photo-of-food-picture-id815617372?k=20&m=815617372&s=612x612&w=0&h=x7kqqTz0aKYSVulHEN-4LkBqUtjWKrl3PKGv0B1m46s="
+        },
+        {
+            id: 3,
+            title: "Be Our Guest",
+            desc: "Be Our Guest is a full stack web application that allows users to write reviews about local restaurants. Users are able to post their reviews, post reactions to reviews made by other users, and to add friends to their profiles.",
+            img: "https://st2.depositphotos.com/2672741/8676/i/600/depositphotos_86765614-stock-photo-gallant-chandelier-with-light-candles.jpg"
+        }
+    ]
+
     return (
         <div className='work' id='work'>
-            Work
+            <div className="slider">
+                {data.map(d => (
+                
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    {/* <img src={d.img} alt=""/> */}
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>
+                                   {d.desc}
+                                    
+                                    </p>
+                                    <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img 
+                            src="https://www.secondnature.com.au/wp-content/uploads/2019/02/igor-miske-174209-unsplash-min-1024x618.jpg" 
+                            alt="" />
+                        </div>
+                    </div>
+                </div>))}
+            </div>
+            <img src="assets/white-arrow-removebg-preview.png" className="arrow left" alt="" />
+            <img src="assets/white-arrow-removebg-preview.png" className="arrow right" alt="" />
+
         </div>
     )
 }
