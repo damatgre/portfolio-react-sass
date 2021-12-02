d2fc33aae81a1524aa0eed1875dc94576083d91e
damatgre <gregdamato2@gmail.com>
2021-12-01 19:58:33 -0500

    initial styling of contact page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import {React, useState} from 'react';
 import "./contact.scss"
 
 export default function Contact () {
+    const [message, setMessage] = useState(false);
+    
+    const handleSubmit=(e)=> {
+        
+        e.preventDefault();
+        setMessage(true);
+    }
+    
     return (
         <div className='contact' id='contact'>
             <div className="left">
-                <img src="assets/phone.png" alt="phone icon"/>
+                <img src="assets/talking.jpg" alt="phone icon"/>
             </div>
 
             <div className="right">
                 <h2>Contact.</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email"></input>
                     <textarea placeholder="Enter your message."></textarea>
                     <button type="submit">Send</button>
+                    {message && <span>Thank you for your message! I will contact you shortly.</span>}
                 </form>
             </div>
         </div>
